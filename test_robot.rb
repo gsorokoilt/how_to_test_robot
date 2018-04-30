682dbb22004d13a3a9c30403cdaa5db04b38b551
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-04-30 15:55:29 -0400

    test 3

--- test_robot.rb
+++ test_robot.rb
@@ -5,30 +5,38 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
+
     # arrange
-
+    #is robot foreign, does it need repairs
     # act
-
+    foreign_damaged = Robot.new
+    foreign_damaged.needs_repairs =  true
+    foreign_damaged.foreign_model = true
     # assert
+    assert_equal(1,foreign_damaged.station)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    #is robot vintage, does it need repairs
     # act
-
+    vintage_damaged = Robot.new
+    vintage_damaged.needs_repairs = true
+    vintage_damaged.vintage_model = true
     # assert
+    assert_equal(2,vintage_damaged.station)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
 
     # act
-
+    standard_damaged = Robot.new
+    standard_damaged.needs_repairs = true
+    standard_damaged.vintage_model = false
+    standard_damaged.foreign_model = false
     # assert
+    assert_equal(3,standard_damaged.station)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
